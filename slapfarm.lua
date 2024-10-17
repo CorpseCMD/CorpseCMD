16daab5d943353e2d1d72e1979e7bf00f77439c0
CorpseCMD <157750559+CorpseCMD@users.noreply.github.com>
2024-10-17 10:44:39 -0500

    Update slapfarm.lua

--- slapfarm.lua
+++ slapfarm.lua
@@ -3,11 +3,11 @@ if time() < 6 then task.wait(3) end
 local Players = game:GetService("Players")
 local lplr = Players.LocalPlayer
 local lchar = lplr.Character or lplr.CharacterAdded:Wait()
+local TARGET = nil
+repeat wait() until lplr:WaitForChild("leaderstats").Glove.Value == "Boxer" or lplr:WaitForChild("leaderstats").Glove.Value == "Dual"
 if lplr:WaitForChild("leaderstats").Glove.Value == "Dual" then
 	lchar:PivotTo(workspace.Lobby.Teleport1.CFrame)
 end
-local TARGET = nil
-repeat wait() until lplr:WaitForChild("leaderstats").Glove.Value == "Boxer" or lplr:WaitForChild("leaderstats").Glove.Value == "Dual"
 for i, v in game.Players:GetChildren() do
    if v ~= lplr and v.Character and v.Character:FindFirstChild("Head") and v.Character:FindFirstChild("Humanoid") and (not v.Character:FindFirstChild("isInArena") or v.Character:FindFirstChild("isInArena").Value == false) then
      TARGET = v
@@ -24,6 +24,7 @@ if TARGET == nil then
 end
 if TARGET ~= nil then
    local args = {[1] = TARGET,[2] = true}
+   task.wait(0.3)
    lchar:PivotTo(TARGET.Character:GetPivot())
    task.wait(0.3)
    lchar:PivotTo(TARGET.Character:GetPivot())
